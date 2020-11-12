34a585a623b7f95acba76a5bc89ac52c99fd0e87
Crystel <crysvane05@gamil.com>
2020-11-11 20:17:26 -0600

    Web

--- proyecto1.0/src/main/java/bases/logIn.java
+++ proyecto1.0/src/main/java/bases/logIn.java
@@ -4,9 +4,18 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.server.ErrorMessage;
+import com.vaadin.server.Resource;
+import com.vaadin.server.ThemeResource;
+import com.vaadin.shared.Connector;
 import com.vaadin.shared.ui.tabsheet.TabsheetState;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 public class logIn extends VerticalLayout implements View {
     TextField usuario;
@@ -56,16 +65,110 @@ public class logIn extends VerticalLayout implements View {
     }
 
     public void banco(){
+        TabSheet principal = new TabSheet();
+        Button boton2 = new Button("Boton2");
+        ArrayList<String> prueba = new ArrayList<>();
+        prueba.add("Parentezco 1");
+        prueba.add("Parentezco 2");
+        prueba.add("Parentezco 3");
 
-        Panel beneficiarios = new Panel("Beneficiarios");
-        Panel estadosCuenta = new Panel("Estados de cuenta");
-        HorizontalLayout lay = new HorizontalLayout(beneficiarios, estadosCuenta);
-        Label bancoL = new Label("BANCO");
-        
-        Panel banco = new Panel(lay);
-        banco.setWidth("100%");
-        banco.setHeight("600px");
-        addComponents(banco);
+
+        //Beneficiarios
+        Accordion beneficiarios = new Accordion();
+
+        //Actualizar beneficiario
+
+        AbsoluteLayout actualizarBeneficiario = new AbsoluteLayout();
+        actualizarBeneficiario.setWidth("1000px");
+        actualizarBeneficiario.setHeight("600px");
+
+        Label datosPersonales = new Label("Datos Personales");
+        TextField nombre = new TextField("Nombre");
+        nombre.setIcon(VaadinIcons.USER_CHECK);
+        TextField cedula = new TextField("Documento identidad");
+        cedula.setIcon(VaadinIcons.TEXT_LABEL);
+        DateField fechaNac = new DateField("Fecha Naacimiento");
+        fechaNac.setIcon(VaadinIcons.CALENDAR_USER);
+
+        Label relPorc = new Label("Relación y porcentaje");
+        ComboBox parentezco = new ComboBox("Parentezo");
+        parentezco.setItems(prueba);
+
+        parentezco.setIcon(VaadinIcons.FAMILY);
+        parentezco.setWidth("300px");
+        TextField porcentaje = new TextField("Porcentaje");
+        porcentaje.setIcon(VaadinIcons.DOLLAR);
+        porcentaje.setWidth("300px");
+
+        Label contacto = new Label("Contacto");
+        TextField email = new TextField("Email");
+        email.setIcon(VaadinIcons.ENVELOPE);
+        TextField tel1 = new TextField("Telefono 1");
+        tel1.setIcon(VaadinIcons.PHONE);
+        TextField tel2 = new TextField("Telefono 2");
+        tel2.setIcon(VaadinIcons.PHONE);
+
+        Button actualizar = new Button("ACTUALIZAR");
+        actualizar.setIcon(VaadinIcons.UPLOAD);
+        actualizar.setStyleName("primary");
+
+        actualizarBeneficiario.addComponent(datosPersonales, "top: 20px; left: 440px");
+        actualizarBeneficiario.addComponent(nombre, "top: 100px; left: 100px");
+        actualizarBeneficiario.addComponent(cedula, "top: 100px; left: 410px");
+        actualizarBeneficiario.addComponent(fechaNac, "top: 100px; right: 100px");
+
+        actualizarBeneficiario.addComponent(relPorc, "top: 200px; left: 430px");
+        actualizarBeneficiario.addComponent(parentezco, "top: 280px; left: 175px");
+        actualizarBeneficiario.addComponent(porcentaje, "top: 280px; right: 175px");
+
+        actualizarBeneficiario.addComponent(contacto, "top: 380px; left: 470px");
+        actualizarBeneficiario.addComponent(email, "top: 460px; left: 100px");
+        actualizarBeneficiario.addComponent(tel1, "top: 460px; left: 410px");
+        actualizarBeneficiario.addComponent(tel2, "top: 460px; right: 100");
+
+        actualizarBeneficiario.addComponent(actualizar, "top: 550px; left: 430px");
+
+        //Agregar beneficiario
+        AbsoluteLayout agregarBeneficiario = new AbsoluteLayout();
+        agregarBeneficiario.setWidth("1000px");
+        agregarBeneficiario.setHeight("500px");
+
+        TextField cedulaA = new TextField("Documento de identidad");
+        cedulaA.setIcon(VaadinIcons.USER_CHECK);
+        ComboBox parentezcoA = new ComboBox("Parentezco");
+        parentezcoA.setIcon(VaadinIcons.FAMILY);
+        parentezco.setItems(prueba);
+        TextField porcentajeA = new TextField("Porcentaje");
+        porcentajeA.setIcon(VaadinIcons.DOLLAR);
+
+        agregarBeneficiario.addComponent(cedulaA, "top: 100px; left: 100px");
+        agregarBeneficiario.addComponent(parentezcoA, "top: 200px; left: 100px");
+        agregarBeneficiario.addComponent(porcentajeA, "top: 300px; left: 100px");
+
+        //Eliminar Beneficiario
+        AbsoluteLayout eliminarBeneficiario = new AbsoluteLayout();
+        eliminarBeneficiario.setHeight("500px");
+        eliminarBeneficiario.setWidth("1000px");
+
+        ComboBox beneficiariosE = new ComboBox("Beneficiarios");
+        beneficiariosE.setIcon(VaadinIcons.GROUP);
+        beneficiariosE.setDescription("HOLA");
+        beneficiariosE.setItems(prueba);
+
+        eliminarBeneficiario.addComponent(beneficiariosE, "top: 100px; left: 100px");
+
+        //Principal
+
+        principal.addTab(beneficiarios,"BENEFICIARIOS");
+        principal.addTab(boton2, "ESTADOS DE CUENTA");
+        beneficiarios.addTab(agregarBeneficiario, "AGREGAR BENEFICIARIO");
+        beneficiarios.addTab(actualizarBeneficiario, "ACTUALIZAR BENEFICIARIO");
+        beneficiarios.addTab(eliminarBeneficiario, "ELIMINAR BENEFICIARIO");
+
+
+        HorizontalLayout lay = new HorizontalLayout(principal);
+        addComponents(lay);
+        setComponentAlignment(lay, Alignment.MIDDLE_CENTER);
 
     }
 
